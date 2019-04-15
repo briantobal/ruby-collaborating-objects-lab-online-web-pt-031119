3eb7255f3376e3bc7e96e68a7def57f170059cb5
Brian Tobal <brian.tobal@gmail.com>
2019-04-15 15:53:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+  attr_accessor :name, :artist 
+
+  def initialize (name)
+    @name = name 
+  end     #def 
+  
+  def self.new_by_filename(file)     
+    artist, song = file.split(" - ") 
+    new_song = self.new(song)
+    new_artist = Artist.find_or_create_by_name(name)
+    new_song.artist = new_artist
+    new_song
+  end     #def 
+
+end
